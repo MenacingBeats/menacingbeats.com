85d0fcccfe29cef47d5621fa6bbd7ff72f0d28a6
Reid <reid@dolbeydev.com>
2020-11-14 07:07:55 +1100

    change main post

--- index.php
+++ index.php
@@ -119,17 +119,17 @@
               </div>
 
               <div class="wrapper">
-				<?php /* Start loop for category 2 'SOTD' */ $query = new WP_Query( 'cat=2&posts_per_page=1' ); ?>
-				<?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
+                <?php /* Start loop for category 2 'SOTD' */ $query = new WP_Query( 'cat=2&posts_per_page=1' ); ?>
+                <?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
 				
 				<div class="post">
 					<h2><?php /* Display the post title */ the_title(); ?></h2>
 					
 					<div class="entry"><?php /* Display the post content */ the_content(); ?></div>
 					
-					<p><small><?php /* Display the tags */ the_tags(); ?></small></p>
+					<div class="tags"><p><small><?php /* Display the tags */ the_tags(); ?></small></p></div>
 					
-					<p class="tags">Artist Facebook: 
+					<p class="artist">Artist Facebook: 
 					<a href="<?php /* Display the artist's socials  */ echo get_post_meta($post->ID, 'facebook-profile', true); ?>">
 					<img alt="Qries" src="https://facebookbrand.com/wp-content/uploads/2019/04/f_logo_RGB-Hex-Blue_512.png?w=512&h=512" width="50" height="50"></a>  
 					</p>
